6acde761112d2baff4c87c91de5e3dad5b468864
JustKira <SHighGamerHG@Gmail.com>
2022-09-05 17:46:22 +0200

    temp env

--- backend/settings.py
+++ backend/settings.py
@@ -16,20 +16,20 @@ from datetime import timedelta
 import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-n!8+nwgm+*a67lj^ed&r4jdx+m$!si%003j^i65&nk5004blg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['gaimiz-backend.herokuapp.com',
-                 '127.0.0.1', env('ALLOWED_HOSTS')]
+                 '127.0.0.1']
 
 AUTH_USER_MODEL = 'customUser.CustomUser'
 # Application definition
